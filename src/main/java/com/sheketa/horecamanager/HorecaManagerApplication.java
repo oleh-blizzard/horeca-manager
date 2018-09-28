d7ce283089f55976f82e36086ec4c8be1bb3bfd4
Oleh Sheketa <metrics114@gmail.com>
2018-09-28 15:26:34 +0300

    Initial commit

---
+++ src/main/java/com/sheketa/horecamanager/HorecaManagerApplication.java
@@ -0,0 +1,12 @@
+package com.sheketa.horecamanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorecaManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HorecaManagerApplication.class, args);
+	}
+}
